d8b20276db6732f9e4eb11e6baf3b040a4cb4946
AndreLeEEEEEE <78389112+AndreLeEEEEEE@users.noreply.github.com>
2021-02-04 14:52:47 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 int main() {
-return 0;
+	// Part A - System Information
+	struct utsname sysInfo;
+
+	printf("System name - %s\n", sysInfo.sysname)
+	printf("System name - %s\n", sysInfo.release)
+	printf("System name - %s\n", sysInfo.version)
+	printf("System name - %s\n", sysInfo.machine)
+	printf("System name - %s\n", sysInfo.nodename)
+	return 0;
+
+	// Part B - Proc Filesystem
+
 }
