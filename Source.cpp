eb4bc106147016183cfa8e1b371dcf521ccc0c93
AndreLeEEEEEE <78389112+AndreLeEEEEEE@users.noreply.github.com>
2021-02-04 16:38:29 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,30 +1,23 @@
 #include <sys/utsname.h>
-#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-int foo() {
-	char buf[32] = { 0x0 };
-	int value = 0;
-	FILE* fd = fopen("/proc/ioboard/input_1", "r");
-	fgets(buf, 32, fd);
-	value = atoi(buf);
-	printf("\n input = %d\n", value);
-	fclose(fd);
-	return value;
-}
-
 int main() {
 	// Part A - System Information
 	struct utsname sysInfo;
 
-	printf("System name - %s\n", sysInfo.sysname);
-	printf("System name - %s\n", sysInfo.release);
-	printf("System name - %s\n", sysInfo.version);
-	printf("System name - %s\n", sysInfo.machine);
-	printf("System name - %s\n", sysInfo.nodename);
+	if (uname(&sysInfo) == -1) {
+		printf("uname failed");
+	}
+	else {
+		printf("System name - %s\n", sysInfo.sysname);
+		printf("System name - %s\n", sysInfo.release);
+		printf("System name - %s\n", sysInfo.version);
+		printf("System name - %s\n", sysInfo.machine);
+		printf("System name - %s\n", sysInfo.nodename);
+	}
 
 	// Part B - Proc Filesystem
 	string line;
